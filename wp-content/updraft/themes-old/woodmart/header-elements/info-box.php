79b0f8f47955e5259806e56dbefabbe14b7a7f6a
Haitham-Zanona <hythamzanona@gmail.com>
2024-12-01 13:34:02 +0200

    1/12/2024

--- wp-content/updraft/themes-old/woodmart/header-elements/info-box.php
+++
@@ -1,9 +0,0 @@
-<?php
-woodmart_enqueue_inline_style( 'header-elements-base' );
-$params['source'] = 'header';
-
-if ( isset( $id ) ) {
-	$params['wrapper_classes'] = ' whb-' . $id;
-}
-
-echo woodmart_shortcode_info_box($params, $params['content']);
